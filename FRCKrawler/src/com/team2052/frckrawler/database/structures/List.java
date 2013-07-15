e274b534acbed2677e38bc6f879120d9b6fd3f17
Charles Hofer <charlie.drummer.hofer@gmail.com>
2013-07-15 12:02:20 -0500

    Forgot to include every file. Woops!

---
+++ FRCKrawler/src/com/team2052/frckrawler/database/structures/List.java
@@ -0,0 +1,46 @@
+package com.team2052.frckrawler.database.structures;
+
+public class List implements Structure {
+	
+	private int eventID;
+	private int listID;
+	private String name;
+	private String description;
+	private Robot[] robots;
+	
+	public List(int _eventID, String _name, String _description) {
+		this(_eventID, _name, _description, new Robot[0]);
+	}
+	
+	public List(int _eventID, String _name, String _description, Robot[] _robots) {
+		this(_eventID, -1, _name, _description, _robots);
+	}
+	
+	public List(int _eventID, int _listID, String _name, String _description, Robot[] _robots) {
+		eventID = _eventID;
+		listID = _listID;
+		name = _name;
+		description = _description;
+		robots = _robots;
+	}
+	
+	public int getEventID() {
+		return eventID;
+	}
+	
+	public int getListID() {
+		return listID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Robot[] getRobots() {
+		return robots;
+	}
+}
---
+++ FRCKrawler/src/com/team2052/frckrawler/gui/StaticTableLayout.java
@@ -0,0 +1,56 @@
+package com.team2052.frckrawler.gui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TableLayout;
+
+import com.team2052.frckrawler.R;
+
+/*****
+ * Class: StaticTableView
+ * 
+ * @author Charles Hofer
+ *
+ * Description: A table that allows for both vertical and horizontal scrolling, but also allows
+ * "static" columns on the left side. Table rows are either added to the main table by calling
+ * the regular addView methods or the addViewToMainTable, or to the static table that does
+ * not scrol horizontally by calling addViewToStaticTable.
+ *****/
+
+public class StaticTableLayout extends FrameLayout {
+	
+	TableLayout mainTable;
+	TableLayout staticSideTable;
+
+	public StaticTableLayout(Context context) {
+		super(context);
+		inflate(context, R.layout.view_static_table_layout, this);
+		mainTable = (TableLayout)findViewById(R.id.mainTable);
+		staticSideTable = (TableLayout)findViewById(R.id.staticSideTable);
+	}
+	
+	public StaticTableLayout(Context context, AttributeSet set) {
+		super(context, set);
+		inflate(context, R.layout.view_static_table_layout, this);
+		mainTable = (TableLayout)findViewById(R.id.mainTable);
+		staticSideTable = (TableLayout)findViewById(R.id.staticSideTable);
+	}
+	
+	public void addView(View child) {
+		mainTable.addView(child);
+	}
+	
+	public void addView(View child, int pos) {
+		mainTable.addView(child, pos);
+	}
+	
+	public void addViewToStaticTable(View child) {
+		staticSideTable.addView(child);
+	}
+	
+	public void addViewToMainTable(View child) {
+		addView(child);
+	}
+}
