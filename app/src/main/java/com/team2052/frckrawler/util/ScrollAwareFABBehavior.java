1f97619a58bc1ff083c09067b4e928d02d396732
Adam Corpstein <Acorpstein8234@gmail.com>
2016-12-23 21:38:26 -0600

    Fix FAB Behavior

--- app/src/main/java/com/team2052/frckrawler/util/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/team2052/frckrawler/util/ScrollAwareFABBehavior.java
@@ -23,7 +23,13 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
-            child.hide();
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
         } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             child.show();
         }
