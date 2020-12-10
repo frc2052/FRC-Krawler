5ba75c384e96e44980df2facde12417122851485
Matt <45442854+matth77@users.noreply.github.com>
2020-12-09 19:50:18 -0600

    Room DB basics

--- app/src/main/java/com/team2052/frckrawler/MainActivity.kt
+++ app/src/main/java/com/team2052/frckrawler/MainActivity.kt
@@ -2,10 +2,22 @@ package com.team2052.frckrawler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.team2052.frckrawler.database.DataBase
+import com.team2052.frckrawler.database.entity.Game
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(applicationContext, DataBase::class.java, "database-name").build()
+
+        lifecycleScope.launch {
+            db.gameDAO().insertOrUpdateGame(Game(0, 2020, "test"));
+        }
+
     }
 }
