9e0f9528709f595b16ca69b0a47386fab3c20613
Bryan Herbst <bryan@team2052.com>
2020-12-14 19:15:47 -0600

    Configure retrofit for TBA API calls

--- app/src/main/java/com/team2052/frckrawler/MainActivity.kt
+++ app/src/main/java/com/team2052/frckrawler/MainActivity.kt
@@ -2,10 +2,21 @@ package com.team2052.frckrawler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.team2052.frckrawler.network.ApiManager
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Temporary to test API calls
+        lifecycleScope.launch {
+            val team = ApiManager.tbaApi.getTeam(2052)
+            Log.d("NetworkingTest", team.toString())
+        }
     }
 }
