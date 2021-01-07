128f063e252ab104ce5feac6d1317f1c58044b33
Matt <45442854+matth77@users.noreply.github.com>
2021-01-06 18:53:00 -0600

    Room db (#65)
    * Room DB basics
    
    * Basic database implementation with a few starter classes.
    
    * Got rid of FRCKrawler class for now. Fixed Android Manifest refrence.
    
    * Remove unwanted .idea files
    
    Co-authored-by: Bryan Herbst <bryan@team2052.com>

--- app/src/main/java/com/team2052/frckrawler/MainActivity.kt
+++ app/src/main/java/com/team2052/frckrawler/MainActivity.kt
@@ -2,15 +2,31 @@ package com.team2052.frckrawler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.team2052.frckrawler.database.DataBase
+import com.team2052.frckrawler.database.table.Event
+import com.team2052.frckrawler.database.table.Game
+import kotlinx.coroutines.launch
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import com.team2052.frckrawler.network.ApiManager
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(applicationContext, DataBase::class.java, "database-name").fallbackToDestructiveMigration().build()
+
+        lifecycleScope.launch {
+            db.gameDAO().insert(Game(0, "test", 2020))
+            db.eventDAO().insert(Event(0, "event", "12/12/12"))
+            val year = db.gameDAO().getGameByYear(2020);
+
+        }
 
 
         // Temporary to test API calls
