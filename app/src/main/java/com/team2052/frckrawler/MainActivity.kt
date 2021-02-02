c9b9e375e0307d16386b33d5c8eecf1cbef40b48
Matt <45442854+matth77@users.noreply.github.com>
2021-02-01 18:22:30 -0600

    This commit add the mode select fragment along with two views.

--- app/src/main/java/com/team2052/frckrawler/MainActivity.kt
+++ app/src/main/java/com/team2052/frckrawler/MainActivity.kt
@@ -9,6 +9,7 @@ import com.team2052.frckrawler.database.table.Event
 import com.team2052.frckrawler.database.table.Game
 import kotlinx.coroutines.launch
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.lifecycleScope
 import com.team2052.frckrawler.network.ApiManager
 import kotlinx.coroutines.launch
@@ -19,15 +20,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(applicationContext, DataBase::class.java, "database-name").fallbackToDestructiveMigration().build()
+        val db = Room.databaseBuilder(applicationContext, DataBase::class.java, "frc_krawler_database").fallbackToDestructiveMigration().build()
 
         lifecycleScope.launch {
             db.gameDAO().insert(Game(0, "test", 2020))
             db.eventDAO().insert(Event(0, "event", "12/12/12"))
             val year = db.gameDAO().getGameByYear(2020);
-
         }
-
 
         // Temporary to test API calls
         lifecycleScope.launch {
