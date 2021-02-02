922e172075351c025aaf178a9d3a0034de88c990
Matt <45442854+matth77@users.noreply.github.com>
2021-02-01 18:22:30 -0600

    This commit add the mode select fragment along with two views.

---
+++ app/src/main/java/com/team2052/frckrawler/views/LabeledSpinner.kt
@@ -0,0 +1,40 @@
+package com.team2052.frckrawler.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
+import com.team2052.frckrawler.R
+import com.team2052.frckrawler.databinding.ExpandableCardBinding
+import com.team2052.frckrawler.databinding.LabeledSpinnerBinding
+
+class LabeledSpinner : ConstraintLayout {
+
+    private val binding: LabeledSpinnerBinding = LabeledSpinnerBinding.inflate(LayoutInflater.from(context), this)
+
+    constructor(context: Context): this(context, null)
+    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs) {
+
+        context.withStyledAttributes(attrs, R.styleable.LabeledSpinner) {
+            binding.spinnerLabel.text = this.getString(R.styleable.LabeledSpinner_spinnerLabel)
+        }
+
+    }
+
+    public fun setLabel(label: String) {
+        binding.spinnerLabel.text = label
+    }
+
+    public fun setOptions(options: List<String>) {
+        val adapter: ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+    }
+
+}
