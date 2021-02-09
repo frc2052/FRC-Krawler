0880395d4468e92d0227b6cb359be61aeb76f38d
Ciocci <43974773+CiocciV@users.noreply.github.com>
2021-02-08 18:13:11 -0600

    Ciocci (#70)
    * made a server fragment, yay
    
    * hooked up mode select with select fragment and server fragment
    
    * added padding on start, top and bottom and fixed the divice_row textview sizes

--- app/src/main/java/com/team2052/frckrawler/modeSelect/ModeSelectFragment.kt
+++ app/src/main/java/com/team2052/frckrawler/modeSelect/ModeSelectFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.team2052.frckrawler.R
 import com.team2052.frckrawler.databinding.ModeSelectFragmentBinding
-import com.team2052.frckrawler.databinding.SampleFragmentBinding
-import com.team2052.frckrawler.sample.SampleScreenState
-import com.team2052.frckrawler.sample.SampleViewModel
-import com.team2052.frckrawler.views.ExpandableCard
-import java.util.zip.Inflater
 
 class ModeSelectFragment : Fragment() {
 
@@ -44,32 +40,7 @@ class ModeSelectFragment : Fragment() {
     private fun render(state: ModeSelectScreenState) {
         when (state) {
             is ModeSelectScreenState.Loading -> {
-//                val remoteScoutCard = ExpandableCard(requireContext());
-//                remoteScoutCard.setHeaderInfo("Remote Scout", "I want to connect to a server and scout")
-//                val remoteScoutDropdown = ArrayList<String>()
-//                remoteScoutDropdown.add("soemthing")
-//                remoteScoutDropdown.add("cool")
-//                remoteScoutDropdown.add("awesome")
-//                remoteScoutCard.addDropdown("Server", remoteScoutDropdown)
-//                binding!!.root.addView(remoteScoutCard)
-//
-//                val serverCard = ExpandableCard(requireContext());
-//                serverCard.setHeaderInfo("Server", "This device will be a server for remote scouts")
-//                val serverDropdown = ArrayList<String>()
-//                serverDropdown.add("soemthing")
-//                serverDropdown.add("cool")
-//                serverDropdown.add("awesome")
-//                serverCard.addDropdown("something", serverDropdown)
-//                binding!!.root.addView(serverCard)
-//
-//                val scoutCard = ExpandableCard(requireContext());
-//                scoutCard.setHeaderInfo("Solo Scouting", "I want to scout without connecting to other devices")
-//                val scoutDropdown = ArrayList<String>()
-//                scoutDropdown.add("soemthing")
-//                scoutDropdown.add("cool")
-//                scoutDropdown.add("awesome")
-//                scoutCard.addDropdown("cool", scoutDropdown)
-//                binding!!.root.addView(scoutCard)
+
             }
 
             is ModeSelectScreenState.Error -> {
@@ -85,6 +56,9 @@ class ModeSelectFragment : Fragment() {
                 binding?.apply {
                     remoteScoutExpandable.addSpinner("server", list)
                     serverExpandable.addSpinner("server", list)
+                    serverExpandable.setContinueButtonListener{
+                        findNavController().navigate(R.id.action_modeSelectFragment_to_serverFragment)
+                    }
                     soloScoutingExpandable.addSpinner("server", list)
                 }
             }
