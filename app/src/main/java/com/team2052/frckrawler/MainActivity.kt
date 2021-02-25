0d7f1fbe0498db077cabf79185f1eef90b855356
Matt <45442854+matth77@users.noreply.github.com>
2021-02-25 16:23:54 -0600

    Added BluetoothManager class to interface with initial bluetooth features.

--- app/src/main/java/com/team2052/frckrawler/MainActivity.kt
+++ app/src/main/java/com/team2052/frckrawler/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.team2052.frckrawler
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
+import com.team2052.frckrawler.bluetooth.BluetoothManager
 import com.team2052.frckrawler.database.DataBase
 import com.team2052.frckrawler.database.table.Event
 import com.team2052.frckrawler.database.table.Game
-import kotlinx.coroutines.launch
-import android.util.Log
-import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
 import com.team2052.frckrawler.network.ApiManager
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    var bluetoothManager: BluetoothManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bluetoothManager = BluetoothManager(baseContext)
+        // Bluetooth requires background location on newer devices
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if(ContextCompat.checkSelfPermission(baseContext, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 0)
+            }
+        }
+        bluetoothManager?.setupBluetoothCapabilities(this)
 
         val db = Room.databaseBuilder(applicationContext, DataBase::class.java, "database-name").fallbackToDestructiveMigration().build()
 
@@ -26,14 +41,34 @@ class MainActivity : AppCompatActivity() {
             db.gameDAO().insert(Game(0, "test", 2020))
             db.eventDAO().insert(Event(0, "event", "12/12/12"))
             val year = db.gameDAO().getGameByYear(2020);
-
         }
-
 
         // Temporary to test API calls
         lifecycleScope.launch {
             val match = ApiManager.tbaApi.getMatches( 2052, "2019mndu2")
             Log.d("NetworkingTest", match.toString())
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            BluetoothManager.BLUETOOTH_ENABLE_REQUEST_CODE -> {
+                when(resultCode) {
+                    Activity.RESULT_OK -> {
+
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        bluetoothManager?.requestBluetoothEnable(this)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bluetoothManager?.cleanup()
     }
 }
---
+++ app/src/main/java/com/team2052/frckrawler/bluetooth/BluetoothInformation.kt
@@ -0,0 +1,6 @@
+package com.team2052.frckrawler.bluetooth
+
+object BluetoothInformation {
+    const val UUID: String = "d6035ed0-8f10-11e2-9e96-0800200c9a66"
+    const val SERVICE_NAME: String = "FRCKrawler"
+}
