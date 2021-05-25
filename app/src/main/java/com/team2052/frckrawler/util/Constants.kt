792a16945d12a85108eb742ef480d5017b680439
Matt <45442854+matth77@users.noreply.github.com>
2021-05-25 18:00:38 -0500

    Implemented tab bar and reworked how screens work.

--- app/src/main/java/com/team2052/frckrawler/util/Constants.kt
+++ app/src/main/java/com/team2052/frckrawler/util/Constants.kt
@@ -1,12 +1,8 @@
 package com.team2052.frckrawler.util
 
+// TODO: potentially put this file in a git-ignore to avoid API_KEY being leaked
 object Constants {
-
     const val BASE_URL = "https://www.thebluealliance.com/api/v3/"
-
-    // TODO: WARNING this needs to be obscured!
     const val API_KEY = "MTnxl1VUPAEseVOl8UGFhM7MjFJfJ1hNMxGd4BF8Lyt3fzqpKkFynemdPMHDyyXA"
-
     const val DATABASE_NAME = "frckrawler.db"
-
 }
