6d55c7f319d1610f543207c210bedd42f164f2b7
Matt <45442854+matth77@users.noreply.github.com>
2021-06-25 15:36:37 -0500

    Gradle Hilt conflict debugging.

--- app/src/main/java/com/team2052/frckrawler/ui/components/FRCKrawlerDrawer.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/components/FRCKrawlerDrawer.kt
@@ -1,11 +1,17 @@
 package com.team2052.frckrawler.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FRCKrawlerDrawer(
     modifier: Modifier = Modifier,
+) = Column(
+    modifier = modifier.padding(24.dp)
 ) {
-
+    Text("Hello world!")
 }
--- app/src/main/java/com/team2052/frckrawler/ui/server/home/ServerHomeScreen.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/server/home/ServerHomeScreen.kt
@@ -1,32 +1,47 @@
 package com.team2052.frckrawler.ui.server.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.team2052.frckrawler.R
 import com.team2052.frckrawler.ui.NavScreen
 import com.team2052.frckrawler.ui.components.*
+import com.team2052.frckrawler.ui.server.ServerViewModel
+import com.team2052.frckrawler.ui.theme.FrcKrawlerTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun ServerHomeScreen(
     modifier: Modifier = Modifier,
-    navController: NavController,
+    navController: NavController = rememberNavController(),
 ) {
+    val viewModel: ServerViewModel = hiltViewModel()
     val scaffoldState = rememberScaffoldState()
-
+    val scope = rememberCoroutineScope()
     FRCKrawlerScaffold(
         modifier = modifier,
         scaffoldState = scaffoldState,
         appBar = {
-            FRCKrawlerAppBar(navController = navController, title = {
-                Text(stringResource(R.string.server_screen_title))
-            })
+            FRCKrawlerAppBar(
+                navController = navController,
+                scaffoldState = scaffoldState,
+                title = {
+                    Text(stringResource(R.string.server_screen_title))
+                }
+            )
         },
         tabBar = {
             FRCKrawlerTabBar(parentNavScreen = NavScreen.Server) { screen ->
@@ -34,33 +49,33 @@ fun ServerHomeScreen(
                     launchSingleTop = true
                 }
             }
-        }
+        },
     ) { contentPadding ->
         ServerHomeScreenContent(
             modifier = Modifier.padding(contentPadding),
+            viewModel = viewModel,
+            navController = navController,
             scaffoldState = scaffoldState,
+            snackbarController = FRCKrawlerSnackbarController(scope),
         )
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ServerHomeScreenContent(
+private fun ServerHomeScreenContent(
     modifier: Modifier = Modifier,
-    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    viewModel: ServerViewModel,
+    navController: NavController,
+    scaffoldState: ScaffoldState,
+    snackbarController: FRCKrawlerSnackbarController,
 ) {
-
-    val snackbarController = FRCKrawlerSnackbarController(rememberCoroutineScope())
-
-//    val viewModel: ServerViewModel = hiltViewModel()
-
-    Text("Server Home Screen", modifier)
-
     FRCKrawlerCard(
         modifier = modifier,
         header = {
             FRCKrawlerCardHeader(
                 title = { Text("Server Properties") },
-                //description = { Text("Control server and configuration") },
+                description = { Text("Control server and configuration") },
             )
         },
         actions = emptyMap(),
@@ -70,71 +85,77 @@ fun ServerHomeScreenContent(
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Button(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(end = 24.dp),
-                onClick = { /*TODO*/ },
+                modifier = Modifier.weight(1f),
+                onClick = {
+                    navController.navigate(NavScreen.ModeSelect.route)
+                },
             ) {
                 Text(text = "Server Config")
             }
+            Spacer(modifier = Modifier.width(24.dp))
             Button(
-                onClick = {
-//                    when (viewModel.serverState.value) {
-//                        ServerState.OFF -> {
-//                            snackbarController.scope.launch {
-//                                snackbarController.showSnackbar(
-//                                    scaffoldState = scaffoldState,
-//                                    message = "starting server...",
-//                                    actionLabel = "dismiss",
-//                                )
-//                            }
-//                        }
-//                        ServerState.ON -> {
-//                            snackbarController.scope.launch {
-//                                snackbarController.showSnackbar(
-//                                    scaffoldState = scaffoldState,
-//                                    message = "stopping server...",
-//                                    actionLabel = "dismiss",
-//                                )
-//                            }
-//                        }
-//                        else -> { }
-//                    }
-//                    viewModel.toggleServer()
-                },
                 modifier = Modifier.weight(0.5f),
+                onClick = {
+                    if (viewModel.serverRunning()) {
+                        viewModel.stopServer()
+                        snackbarController.scope.launch {
+                            snackbarController.showSnackbar(
+                                scaffoldState = scaffoldState,
+                                message = "stopping server...",
+                                actionLabel = "dismiss",
+                            )
+                        }
+                    } else {
+                        viewModel.startServer()
+                        snackbarController.scope.launch {
+                            snackbarController.showSnackbar(
+                                scaffoldState = scaffoldState,
+                                message = "starting server...",
+                                actionLabel = "dismiss",
+                            )
+                        }
+                    }
+                },
             ) {
-//                when (viewModel.serverState.value) {
-//                    ServerState.ON -> {
-//                        Text("Stop Server")
-//                    }
-//                    ServerState.TURNING_ON -> {
-//                        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
-//                            Text("Starting Server")
-//                            Spacer(modifier = Modifier.width(16.dp))
-//                            CircularProgressIndicator(
-//                                modifier = Modifier.size(16.dp),
-//                                color = Color.White.copy(alpha = LocalContentAlpha.current),
-//                                strokeWidth = 2.dp,
-//                            )
-//                        }
-//                    }
-//                    ServerState.OFF -> {
-//                        Text("Start Server")
-//                    }
-//                    ServerState.TURNING_OFF -> {
-//                        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
-//                            Text("Stopping Server")
-//                            Spacer(modifier = Modifier.width(16.dp))
-//                            CircularProgressIndicator(
-//                                modifier = Modifier.size(16.dp),
-//                                color = Color.White.copy(alpha = LocalContentAlpha.current),
-//                                strokeWidth = 2.dp,
-//                            )
-//                        }
-//                    }
-//                }
+                if (viewModel.serverRunning()) {
+                    Text("Stop Server")
+                } else {
+                    Text("Start Server")
+                }
             }
         }
+    }
+    Table(modifier = Modifier) {
+        Text(text = "Scouts", style = MaterialTheme.typography.h6)
+        TableHeader(items = 4) {
+            Text("Col 1")
+            Text("Col 2")
+            Text("Col 3")
+            Text("Col 4")
+        }
+        repeat(4) {
+            TableRow(items = 4) {
+                Text("R- Col 1")
+                Text("R- Col 2")
+                Text("R- Col 3")
+                Text("R- Col 4")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ServerHomeScreenPreviewLight() {
+    FrcKrawlerTheme(darkTheme = false) {
+        ServerHomeScreen()
+    }
+}
+
+@Preview
+@Composable
+private fun ServerHomeScreenPreviewDark() {
+    FrcKrawlerTheme(darkTheme = true) {
+        ServerHomeScreen()
     }
 }
