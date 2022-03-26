f9b0f3be364a052335e1d73541efb9e3193a79ed
Tomas Sepulveda <tomassepulvedagordon@gmail.com>
2022-03-26 15:07:59 -0500

    Made functionality for adding in games, and now the games become buttons that you can click. Need to start implementing the ability for metrics but we're closer.

--- app/src/main/java/com/team2052/frckrawler/ui/server/ServerGamesScreen.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/server/ServerGamesScreen.kt
@@ -1,7 +1,10 @@
 package com.team2052.frckrawler.ui.server
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -16,6 +19,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.team2052.frckrawler.R
 import com.team2052.frckrawler.ui.components.*
+import com.team2052.frckrawler.ui.components.fields.FRCKrawlerTextField
 import com.team2052.frckrawler.ui.navigation.Screen.*
 import com.team2052.frckrawler.ui.theme.FrcKrawlerTheme
 
@@ -24,11 +28,10 @@ fun ServerGamesScreen(
     modifier: Modifier = Modifier,
     navController: NavController,
 ) {
-    val viewModel: ServerViewModel = hiltViewModel()
     val scaffoldState = rememberScaffoldState()
 
     var addGameDialogOpen by remember { mutableStateOf(false) }
-    var fabExpanded by remember { mutableStateOf(false) }
+    var listOfGames by remember { mutableStateOf(emptyList<String>()) }
 
     FRCKrawlerScaffold(
         modifier = modifier,
@@ -51,113 +54,145 @@ fun ServerGamesScreen(
             }
         },
         floatingActionButton = {
-            Column {
-                if (fabExpanded) {
-                    val fabModifier = Modifier.padding(bottom = 24.dp)
-                    FloatingActionButton(
-                        modifier = fabModifier,
-                        onClick = { addGameDialogOpen = true }
-                    ) {
-                        Icon(imageVector = Icons.Filled.Add, contentDescription = "Manual Season Add")
-                    }
-                    FloatingActionButton(
-                        modifier = fabModifier,
-                        onClick = {}
-                    ) {
-                        Icon(imageVector = Icons.Filled.Delete, contentDescription = "Manual Season Add")
-                    }
-                }
-                FloatingActionButton(onClick = {
-                    fabExpanded = !fabExpanded
-                }) {
-                    if (!fabExpanded) {
-                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "Manual Season Add")
-                    } else {
-                        Icon(imageVector = Icons.Filled.Close, contentDescription = "Manual Season Add")
-                    }
-                }
-            }
+            GameActions(
+                onOpen = { addGameDialogOpen = true }
+            )
         },
         drawerContent = {
             FRCKrawlerDrawer()
         },
         background = {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Icon(
-                    modifier = Modifier.size(128.dp),
-                    imageVector = Icons.Filled.Analytics,
-                    contentDescription = "Background",
-                    tint = MaterialTheme.colors.secondary,
-                )
-                Text(text = "No Games", style = MaterialTheme.typography.h4)
+            if (listOfGames.isEmpty()) {
+                EmptyBackground()
             }
         }
     ) { contentPadding ->
         ServerSeasonsScreenContent(
             modifier = Modifier.padding(contentPadding),
-            viewModel = viewModel,
-            navController = navController,
+            listOfGames = listOfGames,
+            onOpen = {}
         )
         if (addGameDialogOpen) {
-            var dialogWidth by remember { mutableStateOf(0) }
-            var gameName by remember { mutableStateOf("") }
-//            AlertDialog(
-//                modifier = Modifier.fillMaxWidth(0.5f),
-//                onDismissRequest = { addGameDialogOpen = false },
-//                title = { Text("Add New Game") },
-//                text = { FRCKrawlerTextField(modifier = Modifier.padding(top = 24.dp), value = gameName, onValueChange = { gameName = it }, label = "Name") },
-//                buttons = {
-//                    ProvideTextStyle(LocalTextStyle.current.copy(color = MaterialTheme.colors.secondary)) {
-//                        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-//                            TextButton(modifier = Modifier.padding(12.dp), onClick = { /*TODO*/ }) {
-//                                Text("CANCEL")
-//                            }
-//                            TextButton(modifier = Modifier.padding(12.dp), onClick = { /*TODO*/ }) {
-//                                Text("SAVE")
-//                            }
-//                        }
-//                    }
-//                }
-//            )
+            AddGameDialog(
+                onAddGame = { newGame -> listOfGames = listOfGames + newGame },
+                onClose = { addGameDialogOpen = false },
+            )
         }
     }
 }
 
 @Composable
+private fun EmptyBackground() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            modifier = Modifier.size(128.dp),
+            imageVector = Icons.Filled.Analytics,
+            contentDescription = "Background",
+            tint = MaterialTheme.colors.secondary,
+        )
+        Text(text = "No Games", style = MaterialTheme.typography.h4)
+    }
+}
+
+@Composable
+private fun GameActions(onOpen: () -> Unit) {
+    var fabExpanded by remember { mutableStateOf(false) }
+    Column {
+        if (fabExpanded) {
+            val fabModifier = Modifier.padding(bottom = 24.dp)
+            FloatingActionButton(
+                modifier = fabModifier,
+                onClick = { onOpen() }
+            ) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "Manual Season Add")
+            }
+            FloatingActionButton(
+                modifier = fabModifier,
+                onClick = {}
+            ) {
+                Icon(imageVector = Icons.Filled.Delete, contentDescription = "Manual Season Add")
+            }
+        }
+        FloatingActionButton(onClick = {
+            fabExpanded = !fabExpanded
+        }) {
+            if (!fabExpanded) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "Manual Season Add")
+            } else {
+                Icon(imageVector = Icons.Filled.Close, contentDescription = "Manual Season Add")
+            }
+        }
+    }
+}
+
+@Composable
+private fun AddGameDialog(
+    onAddGame: (String) -> Unit,
+    onClose: () -> Unit
+) {
+    var gameName by remember { mutableStateOf("") }
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(0.75f),
+        onDismissRequest = { onClose() },
+        title = { Text("Add New Game") },
+        text = {
+            FRCKrawlerTextField(
+                modifier = Modifier.padding(top = 24.dp),
+                value = gameName,
+                onValueChange = { gameName = it },
+                label = "Name"
+            )
+        },
+        buttons = {
+            ProvideTextStyle(LocalTextStyle.current.copy(color = MaterialTheme.colors.secondary)) {
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                    TextButton(
+                        modifier = Modifier.padding(12.dp),
+                        onClick = {
+                            onClose()
+                        }
+                    ) {
+                        Text("CANCEL")
+                    }
+                    TextButton(
+                        modifier = Modifier.padding(12.dp),
+                        onClick = {
+                            onAddGame(gameName)
+                            onClose()
+                        }
+                    ) {
+                        Text("SAVE")
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
 fun ServerSeasonsScreenContent(
     modifier: Modifier = Modifier,
-    viewModel: ServerViewModel,
-    navController: NavController,
+    listOfGames: List<String>,
+    onOpen:() -> Unit
 ) {
-    Column(modifier = Modifier.background(color = MaterialTheme.colors.background.copy(alpha = 0.5f))) {
-        var highlighted by remember { mutableStateOf(false) }
-        val txtMod = Modifier.padding(12.dp)
-
-        val checkedStates = remember { mutableStateListOf(false, false, false, false, false) }
-//        FRCKrawlerDataTable(
-//            dataTableSource = TableSource(
-//                TableRow({ Text("Year") }, { Text("Season") }, { Text("Metrics") }, checked = checkedStates[0]),
-//                TableRow({ Text("2020") }, { Text("Infinite Recharge") }, { Text("true") }, checked = checkedStates[1]),
-//                TableRow({ Text("2019") }, { Text("Power Up") }, { Text("false") }, checked = checkedStates[2]),
-//                TableRow({ Text("2018") }, { Text("Rover Ruckus") }, { Text("false") }, checked = checkedStates[3]),
-//                TableRow({ Text("2020") }, { Text("Infinite Recharge") }, { Text("true") }, checked = checkedStates[1]),
-//                TableRow({ Text("2019") }, { Text("Power Up") }, { Text("false") }, checked = checkedStates[2]),
-//                TableRow({ Text("2018") }, { Text("Rover Ruckus") }, { Text("false") }, checked = checkedStates[3]),
-//                TableRow({ Text("2020") }, { Text("Infinite Recharge") }, { Text("true") }, checked = checkedStates[1]),
-//                TableRow({ Text("2019") }, { Text("Power Up") }, { Text("false") }, checked = checkedStates[2]),
-//                TableRow({ Text("2018") }, { Text("Rover Ruckus") }, { Text("false") }, checked = checkedStates[3]),
-//                TableRow({ Text("2020") }, { Text("Infinite Recharge") }, { Text("true") }, checked = checkedStates[1]),
-//                TableRow({ Text("2019") }, { Text("Power Up") }, { Text("false") }, checked = checkedStates[2]),
-//                TableRow({ Text("2018") }, { Text("Rover Ruckus") }, { Text("false") }, checked = checkedStates[3]),
-//            ),
-//            onCheckedChange = { index, checked ->
-//                checkedStates[index] = checked
-//            }
-//        )
+    Column(modifier = modifier.fillMaxWidth()) {
+        listOfGames.forEach { game ->
+            TextButton(
+                modifier = Modifier.padding(12.dp),
+                onClick = {
+                    onOpen()
+                }
+            ) {
+                Text(
+                    text = game,
+                    style = MaterialTheme.typography.h4
+                )
+            }
+        }
     }
 }
 
