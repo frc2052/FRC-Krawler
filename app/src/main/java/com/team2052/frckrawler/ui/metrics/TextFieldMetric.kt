ece2311fa2a1f04b0cd30fea7fd44ab5649bab33
Bryan Herbst <bryan@team2052.com>
2024-01-20 09:18:22 -0600

    Sentence case text field input

--- app/src/main/java/com/team2052/frckrawler/ui/metrics/TextFieldMetric.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/metrics/TextFieldMetric.kt
@@ -2,10 +2,12 @@ package com.team2052.frckrawler.ui.metrics
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import com.team2052.frckrawler.data.model.Metric
 import com.team2052.frckrawler.ui.FrcKrawlerPreview
 import com.team2052.frckrawler.ui.theme.FrcKrawlerTheme
@@ -18,7 +20,10 @@ fun TextFieldMetric(
   TextField(
     modifier = Modifier.fillMaxWidth(),
     value = state,
-    onValueChange = onStateChanged
+    onValueChange = onStateChanged,
+    keyboardOptions = KeyboardOptions(
+      capitalization = KeyboardCapitalization.Sentences
+    )
   )
 }
 
