61a9515a6845683b3ea0db0f4335883122e23e0f
Bryan Herbst <bryan@team2052.com>
2025-03-11 14:15:53 -0500

    Show connected scouts in list

--- app/src/main/java/com/team2052/frckrawler/bluetooth/server/ConnectedScoutObserver.kt
+++ app/src/main/java/com/team2052/frckrawler/bluetooth/server/ConnectedScoutObserver.kt
@@ -2,19 +2,18 @@ package com.team2052.frckrawler.bluetooth.server
 
 import com.team2052.frckrawler.data.model.RemoteScout
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.EmptyCoroutineContext
 
 @Singleton
 class ConnectedScoutObserver @Inject constructor() {
-  private val scope = CoroutineScope(EmptyCoroutineContext)
+  private val scope = CoroutineScope(Dispatchers.Default)
   private val _devices = MutableStateFlow<Map<String, RemoteScout>>(emptyMap())
 
   /**
@@ -27,7 +26,7 @@ class ConnectedScoutObserver @Inject constructor() {
    */
   internal fun notifyScoutSynced(name: String, address: String) {
     scope.launch {
-      val deviceMap = _devices.last().toMutableMap()
+      val deviceMap = _devices.value.toMutableMap()
       val syncedScout = deviceMap[address]
         ?.copy(lastSync = LocalDateTime.now())
         ?: RemoteScout(
--- app/src/main/java/com/team2052/frckrawler/ui/server/home/ConnectedScoutsList.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/server/home/ConnectedScoutsList.kt
@@ -2,19 +2,30 @@ package com.team2052.frckrawler.ui.server.home
 
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.team2052.frckrawler.R
 import com.team2052.frckrawler.data.model.RemoteScout
 import com.team2052.frckrawler.ui.components.Card
 import com.team2052.frckrawler.ui.components.CardHeader
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.Month
 import java.time.format.DateTimeFormatter
 
 @Composable
@@ -30,21 +41,29 @@ internal fun ConnectedScoutsList(
       )
     },
   ) {
-    val context = LocalContext.current
-    Button(
-      onClick = {
-        val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
-          putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60)
-        }
-        context.startActivity(discoverableIntent)
-      }
-    ) {
-      Text(stringResource(R.string.server_connect_new_scouts_button))
-    }
-
     Column {
       scouts.forEach { scout ->
         SyncedScout(scout)
+        Spacer(Modifier.height(8.dp))
+      }
+    }
+
+    Spacer(Modifier.height(8.dp))
+
+    Box(
+      modifier = Modifier.fillMaxWidth(),
+      contentAlignment = Alignment.BottomEnd
+    ) {
+      val context = LocalContext.current
+      Button(
+        onClick = {
+          val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60)
+          }
+          context.startActivity(discoverableIntent)
+        }
+      ) {
+        Text(stringResource(R.string.server_connect_new_scouts_button))
       }
     }
   }
@@ -68,4 +87,39 @@ private fun SyncedScout(
       fontStyle = FontStyle.Italic
     )
   }
+}
+
+@Preview
+@Composable
+private fun ConnectedScoutListPreview() {
+  ConnectedScoutsList(
+    scouts = listOf(
+      RemoteScout(
+        name = "Scout 1",
+        address = "AB:CD:EF:12:34:56",
+        lastSync = LocalDateTime.of(
+          LocalDate.of(2025, Month.MARCH, 10),
+          LocalTime.of(8, 52)
+        )
+      ),
+
+      RemoteScout(
+        name = "Scout 2",
+        address = "AB:CD:EF:12:34:56",
+        lastSync = LocalDateTime.of(
+          LocalDate.of(2025, Month.MARCH, 10),
+          LocalTime.of(8, 52)
+        )
+      ),
+
+      RemoteScout(
+        name = "Scout 3",
+        address = "AB:CD:EF:12:34:56",
+        lastSync = LocalDateTime.of(
+          LocalDate.of(2025, Month.MARCH, 10),
+          LocalTime.of(8, 52)
+        )
+      )
+    )
+  )
 }
