f2965682e4fab7f1c2e4786ce81dd82b2e9a6e70
Bryan Herbst <bryan@team2052.com>
2025-03-14 15:22:25 -0500

    Allow remote scout and server home screens to scroll

--- app/src/main/java/com/team2052/frckrawler/ui/scout/remote/RemoteScoutHomeScreen.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/scout/remote/RemoteScoutHomeScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.EmojiEvents
 import androidx.compose.material.icons.filled.Hardware
@@ -27,7 +29,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -128,7 +129,10 @@ private fun ScoutHomeScreenContent(
   modifier: Modifier = Modifier,
   serverStatus: @Composable () -> Unit,
 ) {
-  Column(modifier = modifier.padding(spaceLarge)) {
+  Column(modifier = modifier
+    .verticalScroll(rememberScrollState())
+    .padding(spaceLarge)
+  ) {
     serverStatus()
 
     Spacer(Modifier.height(16.dp))
--- app/src/main/java/com/team2052/frckrawler/ui/server/home/ServerHomeScreen.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/server/home/ServerHomeScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -65,7 +67,9 @@ fun ServerHomeScreen(
       },
     ) { contentPadding ->
       Column(
-        modifier = Modifier.padding(contentPadding)
+        modifier = Modifier
+          .verticalScroll(rememberScrollState())
+          .padding(contentPadding)
           .consumeWindowInsets(contentPadding)
           .padding(spaceLarge)
       ) {
