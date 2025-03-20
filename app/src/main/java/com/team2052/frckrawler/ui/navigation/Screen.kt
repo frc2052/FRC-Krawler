9bbd4dfe9fab95e9b89c1d6e8b8050485077ec23
Bryan Herbst <bryan@team2052.com>
2025-03-20 16:02:47 -0500

    Remove old screen title code

--- app/src/main/java/com/team2052/frckrawler/ui/navigation/Screen.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/navigation/Screen.kt
@@ -3,8 +3,6 @@ package com.team2052.frckrawler.ui.navigation
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
-import com.team2052.frckrawler.FRCKrawlerApp.Companion.getString
-import com.team2052.frckrawler.R
 
 /**
  * Represents a unique screen consisting of properties for the route, title, and sub-screens
@@ -13,29 +11,21 @@ import com.team2052.frckrawler.R
  */
 sealed class Screen(
   val route: String,
-  val title: String,
   val screens: List<Screen> = emptyList(),
   val arguments: List<NamedNavArgument> = emptyList(),
 ) {
 
   // Mode selection screen
-  data object ModeSelect : Screen(
-    "mode_select_screen",
-    getString(R.string.mode_select_screen_title),
-  )
+  data object ModeSelect : Screen("mode_select_screen")
 
   // Scouting screens
-  data object RemoteScoutHome : Screen(
-    "scout/remote",
-    getString(R.string.scout_screen_title),
-  )
+  data object RemoteScoutHome : Screen("scout/remote")
 
   data class MatchScout(
     val eventId: Int? = null,
     val metricSetId: Int? = null,
   ) : Screen(
     "scout/match/${eventId ?: "{eventId}"}/${metricSetId ?: "{metricSetId}"}",
-    getString(R.string.scout_matches_screen_title),
     arguments = listOf(Arguments.eventId, Arguments.metricSetId)
   )
 
@@ -44,14 +34,10 @@ sealed class Screen(
     val metricSetId: Int? = null,
   ) : Screen(
     "scout/pit/${eventId ?: "{eventId}"}/${metricSetId ?: "{metricSetId}"}",
-    getString(R.string.scout_matches_screen_title),
     arguments = listOf(Arguments.eventId, Arguments.metricSetId)
   )
 
-  data object GameList : Screen(
-    "games",
-    getString(R.string.games_screen_title)
-  )
+  data object GameList : Screen("games" )
 
   /* TODO clean up this nasty argument business. Args should be non-null */
   data class Server(
@@ -59,7 +45,6 @@ sealed class Screen(
     val eventId: Int? = null,
   ) : Screen(
     "server/${gameId ?: "{gameId}"}/${eventId ?: "{eventId}"}",
-    getString(R.string.server_screen_title),
     arguments = listOf(Arguments.gameId, Arguments.eventId)
   )
 
@@ -68,7 +53,6 @@ sealed class Screen(
     val eventId: Int? = null,
   ) : Screen(
     "analyze/${gameId ?: "{gameId}"}/${eventId ?: "{eventId}"}",
-    getString(R.string.server_screen_title),
     arguments = listOf(Arguments.gameId, Arguments.eventId)
   )
 
@@ -77,25 +61,21 @@ sealed class Screen(
     val eventId: Int? = null,
   ) : Screen(
     "export/${gameId ?: "{gameId}"}/${eventId ?: "{eventId}"}",
-    getString(R.string.server_screen_title),
     arguments = listOf(Arguments.gameId, Arguments.eventId)
   )
 
   data class Game(val gameId: Int? = null) : Screen(
     route = "game/${gameId ?: "{gameId}"}",
-    title = "",
     arguments = listOf(Arguments.gameId)
   )
 
   data class MetricSet(val metricSetId: Int? = null) : Screen(
     route = "metric_set/${metricSetId ?: "{metricSetId}"}",
-    title = getString(R.string.metrics_screen),
     arguments = listOf(Arguments.metricSetId)
   )
 
   data class Event(val eventId: Int? = null) : Screen(
     route = "event/${eventId ?: "{eventId}"}",
-    title = "",
     arguments = listOf(Arguments.eventId)
   )
 }
