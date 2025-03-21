7f2b5a96d3e70c3e683d475d12684982c1d8bfc7
Bryan Herbst <bryan@team2052.com>
2025-03-21 14:56:33 -0500

    Cleanup theme.kt

--- app/src/main/java/com/team2052/frckrawler/ui/theme/Theme.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/theme/Theme.kt
@@ -1,15 +1,10 @@
 package com.team2052.frckrawler.ui.theme
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -90,7 +85,7 @@ private val darkScheme = darkColorScheme(
 @Composable
 fun FrcKrawlerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
   val colorScheme = when {
       darkTheme -> darkScheme
