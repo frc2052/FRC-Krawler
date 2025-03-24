8aa35b0cd6ec2945db171dea0fdd4cec6334e18d
Bryan Herbst <bryan@team2052.com>
2025-03-24 15:30:25 -0500

    Allow scrolling in analyze filter sheet

--- app/src/main/java/com/team2052/frckrawler/ui/analyze/AnalyzeDataFilterSheet.kt
+++ app/src/main/java/com/team2052/frckrawler/ui/analyze/AnalyzeDataFilterSheet.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -52,7 +54,9 @@ fun AnalyzeDataFilterSheet(
     sheetState = sheetState,
     onDismissRequest = onDismissRequest,
   ) {
-    Column {
+    Column(
+      modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
       filterState.availableMetrics.forEach { metric ->
         MetricRow(
           metric = metric,
